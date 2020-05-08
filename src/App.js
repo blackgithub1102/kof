9b379254e40ba8cdff3796e957a29785ba1a597c
Họa Kỵ <maxanlee1998@gmail.com>
2020-05-08 12:45:32 +0700

    Change somethings

--- src/App.js
+++ src/App.js
@@ -9,6 +9,26 @@ const HatsPage = () => (
     <h1>HATS PAGE</h1>
   </div>
 )
+const JacketsPage = () => (
+  <div>
+    <h1>JACKETS PAGE</h1>
+  </div>
+)
+const WomensPage = () => (
+  <div>
+    <h1>WOMENS PAGE</h1>
+  </div>
+)
+const SneakersPage = () => (
+  <div>
+    <h1>SNEAKERS PAGE</h1>
+  </div>
+)
+const MensPage = () => (
+  <div>
+    <h1>MENS PAGE</h1>
+  </div>
+)
 
 function App() {
   return (
@@ -16,6 +36,10 @@ function App() {
       <Switch>
         <Route exact path='/' component={ HomePage } />
         <Route path='/hats' component={ HatsPage } />
+        <Route path='/jackets' component={ JacketsPage } />
+        <Route path='/sneakers' component={ SneakersPage } />
+        <Route path='/womens' component={ WomensPage } />
+        <Route path='/mens' component={ MensPage } />
       </Switch>
     </div>
   );
