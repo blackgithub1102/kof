b120955165822b1879fe7d4b034ee8176ffa4806
Họa Kỵ <maxanlee1998@gmail.com>
2020-05-08 12:25:45 +0700

    Updates

---
+++ precache-manifest.fb0468109469ddede57368b9878504de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42291ec5aaa76b831018e7fae537f407",
+    "url": "/index.html"
+  },
+  {
+    "revision": "30c80e1b62f3dda423ac",
+    "url": "/static/css/main.3bd05f71.chunk.css"
+  },
+  {
+    "revision": "9e8ed930858392bdc409",
+    "url": "/static/js/2.cfd9582a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.cfd9582a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30c80e1b62f3dda423ac",
+    "url": "/static/js/main.212b2dd2.chunk.js"
+  },
+  {
+    "revision": "2060b9a7e54a59ab7b07",
+    "url": "/static/js/runtime-main.f3a02c50.js"
+  }
+]);
