ecaad7e5a53015a83fe8734a6e29709c4e6d75e8
Họa Kỵ <maxanlee1998@gmail.com>
2020-05-08 12:26:33 +0700

    Updates

---
+++ precache-manifest.95b0bfde7f4fac46a4db6e11748261d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5637013126f4ef0245a790b28ad54c57",
+    "url": "/kof/index.html"
+  },
+  {
+    "revision": "30c80e1b62f3dda423ac",
+    "url": "/kof/static/css/main.3bd05f71.chunk.css"
+  },
+  {
+    "revision": "9e8ed930858392bdc409",
+    "url": "/kof/static/js/2.cfd9582a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/kof/static/js/2.cfd9582a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30c80e1b62f3dda423ac",
+    "url": "/kof/static/js/main.212b2dd2.chunk.js"
+  },
+  {
+    "revision": "ed95e94ed52587c333ba",
+    "url": "/kof/static/js/runtime-main.eff72f16.js"
+  }
+]);
